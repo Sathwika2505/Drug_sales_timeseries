b167f53c8626a77a6f23d61c0def8c08585c29cb
Sathwika2505 <142480146+Sathwika2505@users.noreply.github.com>
2024-08-20 11:01:52 +0530

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,31 +1,16 @@
 import pandas as pd
 
 def data_extraction():
-    daily = pd.read_csv("salesdaily.csv")
-    weekly = pd.read_csv("salesweekly.csv")
-    monthly = pd.read_csv("salesmonthly.csv")
-    print("daily :",daily)
-    print("weekly :",weekly)
+    monthly = pd.read_csv("/home/ubuntu/Object_detection_FCOS/Drugsales/salesmonthly.csv")
     print("monthly :",monthly)
-    
-    #converting datatype of dates from object to Datetime
     monthly['datum'] = pd.to_datetime(monthly['datum'], format= '%Y-%m-%d')
-    weekly['datum'] = pd.to_datetime(weekly['datum'], format= '%m/%d/%Y')
-    daily['datum'] = pd.to_datetime(daily['datum'], format= '%m/%d/%Y')
     
     print("daily :",monthly['datum'])
-    print("weekly :",weekly['datum'])
-    print("monthly :",daily['datum'])
-    
-    monthly['year'] = monthly['datum'].dt.year
     monthly['month'] = monthly['datum'].dt.month
-    monthly['day'] = monthly['datum'].dt.day
     
     print("daily :",monthly['datum'])
-    print("weekly :",weekly['datum'])
-    print("monthly :",daily['datum'])
     
-    return daily, weekly, monthly
+    return monthly
     
 data_extraction()
 
