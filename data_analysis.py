17473780534667b37c2a59b705bd000d4a2bc740
Sathwika2505 <142480146+Sathwika2505@users.noreply.github.com>
2024-08-20 11:02:10 +0530

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -1,18 +1,13 @@
 from data_extraction import data_extraction
 
 def data_analysis():
-    daily, weekly, monthly=data_extraction()
-#    print("daily :",daily)
-#    print("weekly :",weekly)
-#    print("monthly :",monthly)
-    merged_data = daily.merge(weekly, how='outer', left_index=True, right_index=True, suffixes=('_daily', '_weekly'))
-    merged_data = merged_data.merge(monthly, how='outer', left_index=True, right_index=True, suffixes=('', '_monthly'))
-    merged_data.fillna(method='ffill', inplace=True)
-    print(merged_data.head())
-    print(merged_data.tail())
-    print(merged_data.describe())
-    print ("Missing values\n\n",merged_data.isnull().any(),sep='')
-    print (merged_data.info())
-    return merged_data
+    monthly=data_extraction()
+    print("monthly :",monthly)
+    print(monthly.head())
+    print(monthly.tail())
+    print(monthly.describe())
+    print ("Missing values\n\n",monthly.isnull().any(),sep='')
+    print (monthly.info())
+    return monthly
 
 data_analysis() 
