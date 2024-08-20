6d1460506113556f6c1a0967fcc358221a790193
Sathwika2505 <142480146+Sathwika2505@users.noreply.github.com>
2024-08-20 11:25:50 +0530

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -7,7 +7,12 @@ from pandas.api.types import is_numeric_dtype
 def feature_engineering():
 
     data = data_analysis()
+    print(data)
     #data = data.drop(columns=['Asset_ID', 'Asset_Type'],axis=1)
+    data['datum'] = pd.to_datetime(data['datum'])
+
+    # Set 'datum' as the index if it's not already
+    data.set_index('datum', inplace=True)
     print(data.head())
     data.to_csv("cleaned_data.csv",index=True)
     return data
